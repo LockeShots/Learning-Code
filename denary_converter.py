6ef90b79ffe27df6bf76991bc002f2fd27cfaa57
Locke <mattjones@harmonyinautumn.com>
2020-12-28 05:47:44 +0000

    Working

--- denary_converter.py
+++ denary_converter.py
@@ -1,28 +1,14 @@
-#def Dec2Bin(denary):
-#  output = ""
-#  while denary > 0:
-#    remainder = denary % 2
-#    output = str(remainder) + output
-#    denary = int((denary - remainder) / 2)
-#  return output
-  
-#thenumber = int(input('enter a number to convert: '))
-#print(f"Binary value = {Dec2Bin(thenumber)}")
-
-"""
->>> Bin2Dec(1)
-1
-"""
-
 def Bin2Dec(binary):
     output = 0
     operand = 1
     index = -1
-    while index <= len(binary):
-        output = binary[(index)] * operand
+    outputlist = []
+    while abs(index) <= len(binary):
+        output = int(binary[(index)]) * operand
+        outputlist.append(output)
         index = index - 1
         operand = operand * 2
-    return output
+    return sum([int(i) for i in outputlist])
 
 binaryvalue = (input('Enter a binary number to convert to denary: '))
 print(f"Denary value = {Bin2Dec(binaryvalue)}")
