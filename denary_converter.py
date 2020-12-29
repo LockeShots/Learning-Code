3c6d9e247c980394fa1c90bec1f3b10c28d48af7
unknown <MattJones@harmonyinautumn.com>
2020-12-29 13:52:38 +0000

    Optimisations!

--- denary_converter.py
+++ denary_converter.py
@@ -1,14 +1,21 @@
-def Bin2Dec(binary):
+#Converts a binary value into a denary value
+def Bin2Dec(binaryvalue):
+    """
+    >>> Bin2Dec('1101')
+    11
+    >>> Bin2Dec('11101')
+    23
+    """
     output = 0
-    operand = 1
-    index = -1
-    outputlist = []
-    while abs(index) <= len(binary):
-        output = int(binary[(index)]) * operand
-        outputlist.append(output)
-        index = index - 1
-        operand = operand * 2
-    return sum([int(i) for i in outputlist])
+    factor = 1
+    #Progresses through the reversed binary string and multiplies each index by the factor
+    for i in range(len(binaryvalue)):
+        output += int(binaryvalue[i]) * factor
+    #Increases the factor by a power of 2 each loop
+        factor = factor * 2
+    return output
 
-binaryvalue = (input('Enter a binary number to convert to denary: '))
-print(f"Denary value = {Bin2Dec(binaryvalue)}")
+if __name__ == '__main__':
+    #Takes user inport and reverses it
+    binaryvalue = (input('Enter a binary number to convert to denary: ')[::-1])
+    print(f"Denary value = {Bin2Dec(binaryvalue)}")
