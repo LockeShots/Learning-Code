cb3a6d541dd0643459d58446b880cb029b5b332f
unknown <MattJones@harmonyinautumn.com>
2020-12-30 04:06:59 +0000

    Optimisation 1

--- hex_to_denary_converter.py
+++ hex_to_denary_converter.py
@@ -1,20 +1,20 @@
 hexdic = {
-        '0':'0',
-        '1':'1',
-        '2':'2',
-        '3':'3',
-        '4':'4',
-        '5':'5',
-        '6':'6',
-        '7':'7',
-        '8':'8',
-        '9':'9',
-        'A':'10',
-        'B':'11',
-        'C':'12',
-        'D':'13',
-        'E':'14',
-        'F':'15',
+        '0':0,
+        '1':1,
+        '2':2,
+        '3':3,
+        '4':4,
+        '5':5,
+        '6':6,
+        '7':7,
+        '8':8,
+        '9':9,
+        'A':10,
+        'B':11,
+        'C':12,
+        'D':13,
+        'E':14,
+        'F':15,
     }
 
 def Hex2Dec(hexvalue):
@@ -25,7 +25,7 @@ def Hex2Dec(hexvalue):
         hex2decvalue.append(hexdic[hexvalue[i]])
 
     for i in range(len(hex2decvalue)):  
-        output += int(hex2decvalue[i]) * (16**i)
+        output += hex2decvalue[i] * (16**i)
     return output
 
 if __name__ == '__main__':
