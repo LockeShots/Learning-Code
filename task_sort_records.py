9eb0d718163e7625208030e4dd8047e8d82680aa
Locke <mattjones@harmonyinautumn.com>
2021-03-05 13:46:57 +0000

    21.03.05 Long-winded

--- task_sort_records.py
+++ task_sort_records.py
@@ -1,17 +1,16 @@
 listofdicts = []
-f = open("project_recordsearch_data.csv", 'r')
-for line in f:
-    d = {}
+csv = open("project_recordsearch_data.csv", 'r')
+for line in csv:
+    csvdict = {}
     worklist = line.split("|")
     index = 0
     while index < len(worklist):
       worklist[index] = worklist[index].strip()
       index += 1
-    #print (worklist)
-    d.update({"First Name": worklist[0]})
-    d.update({"Surname": worklist[1]})
-    d.update({"Date of Birth": worklist[2]})
-    d.update({"Address": worklist[3]})
-    listofdicts.append(d)
+    csvdict.update({"First Name": worklist[0]})
+    csvdict.update({"Surname": worklist[1]})
+    csvdict.update({"Date of Birth": worklist[2]})
+    csvdict.update({"Address": worklist[3]})
+    listofdicts.append(csvdict)
     
 print (listofdicts)
